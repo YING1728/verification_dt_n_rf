f6ac603f318879336ac7c4312ff28455c4187ef4
YING1728 <55714026+YING1728@users.noreply.github.com>
2019-09-30 18:17:19 +0200

    Update tree_n_paths.py

--- paths_generation/tree_n_paths.py
+++ paths_generation/tree_n_paths.py
@@ -2,11 +2,9 @@
 # before running this file, data should be extracted as csv file and be stored under ../data_generation/tables/name.csv
 
 import pandas as pd
-import numpy as np
 import pickle # using to save and load object
-
-from sklearn.tree import _tree
 from sklearn import tree
+from toPath_Con import tree_to_path
 
 
 # get data from csv, for example "../data_generation/tables/table_ACASXU_run2a_1_1_batch_2000.csv"
@@ -17,52 +15,6 @@ def get_data():
     data = pd.read_csv("../data_generation/tables/table_ACASXU_run2a_1_1_batch_2000.csv",
                        names=['f0', 'f1', 'f2', 'f3', 'f4', 'l_min', 'l_max'])
     return data
-
-
-def tree_to_path(tree, feature_names):
-    paths = []
-    path = []
-    tree_ = tree.tree_
-    feature_name = [
-        feature_names[i] if i != _tree.TREE_UNDEFINED  else "undefined!"   # no feature name of this node, means leaf nodes.
-        for i in tree_.feature
-    ]
-
-    def store_path(node, path_a):
-
-        if tree_.feature[node] != _tree.TREE_UNDEFINED: #if not a leaf node, decision conditions like f0 > 0.5.
-            name = feature_name[node]
-            threshold = tree_.threshold[node]
-            path_temp1 = path_a[:]
-            path_temp2 = path_a[:]
-            # add the constraint to the left
-            con_left = "{} <= {}".format(name, threshold)
-            path_temp1.append(con_left)
-            store_path(tree_.children_left[node], path_temp1)
-            # add the constraint to the right
-            con_right = "{} > {}".format(name, threshold)
-            path_temp2.append(con_right)
-            store_path(tree_.children_right[node], path_temp2)
-
-        else:
-            path_temp3 = path_a[:]
-            # value of scikit learn decision tree means the current number of each classes
-            # (2, 0) means there are two element of the first class; (0, 2) means there two element of the second class.
-            # the following maps tree leaf value to the corresponding class name.
-
-            # get index that with nonzero value
-            index = np.nonzero(tree_.value[node][0])
-
-            leaf_label = tree.classes_[index][0].split(',')  # got format like ['3.0', '1.0']
-            label_min = 'lmin == ' + str(leaf_label[0])
-            label_max = 'lmax == ' + str(leaf_label[1])
-            # add the leaf to the end of the path.
-            path_temp3.append(label_min)
-            path_temp3.append(label_max)
-            paths.append(path_temp3)
-
-    store_path(0,  path)
-    return paths
 
 
 def main():
@@ -88,7 +40,7 @@ def main():
     # print class names of the tree (unique leaves).
     print(clf.classes_)
 
-    tree_paths = tree_to_path(clf, features)
+    tree_paths = tree_to_path(clf, features, clf.classes_)
 
     # store the tree to a text file, for example for neural network 1_1 save to txt file "tree_paths_11.txt"
     with open("paths_dt/tree_paths_11.txt", "wb") as fp:
